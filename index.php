a23c81dcd80f1395ab3edd8130d6558a2ff362f8
sylviekutz <sylviekutz@gmail.com>
2020-02-06 19:15:36 +0100

    correction PDO-PARTIE2-Exercice4

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+    <!-- Bootstrap CSS -->
+        <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/lumen/bootstrap.min.css" rel="stylesheet" integrity="sha384-715VMUUaOfZR3/rWXZJ9agJ/udwSXGEigabzUbJm2NR4/v5wpDy8c14yedZN6NL7" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+  <title>Partie 2 | PDO</title>
+</head>
+<body>
+  <div class="container-fluid box-link">
+      <img>
+     <a href="http://pdo/partie2/index.php/"></a>
+  </div>
+  <div class="exercice1P2 container mt-4 pb-3 d-flex justify-content-around"  style="border: solid 1px #fff;">
+    <div class="row">
+      <div class="col-md-12">
+        <h2>Page d'accueil</h2>
+      </div>
+      <div class="col-md-6">
+        <a class="btn btn-warning" href="ajout-patient.php">Créer un patient</a>
+      </div>
+      <div class="col-md-6">
+        <a class="btn btn-light" href="liste-patients.php">Liste des patients</a>
+      </div>
+    </div>
+  </div>
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+    </body>
+</html>
